4d615443f5a02a7024c0cddc5edcfd9319782462
anuj1611 <157007144+anuj1611@users.noreply.github.com>
2024-02-08 12:57:48 +0000

    todo_app

--- app.py
+++ app.py
@@ -1,7 +1,77 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import sys
 
-app = Flask(__name__)
+app= Flask(__name__)
+
+todo_list_global=[]
+count=0
+
+class Todo:
+    def __init__(self,num,title):
+        self.id=num
+        self.title= title
+        self.complete=False
+
+    def __str__(self):
+        return f"{self.title}"
+    
+
+    def updateComplete(self):
+        self.complete= True
+        return self.complete
+
+    def updateNotComplete(self):
+        self.complete= False
+        return self.complete
+    
+    def updateid(self, newid):
+        self.id= newid
+    
+    def delete(self):
+        global todo_list_global
+        todo_list_global = [todo for todo in todo_list_global if todo.id != self.id]
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    global todo_list_global
+    return render_template("index.html", todo_list= todo_list_global)
+
+@app.route("/add", methods=["POST"])
+def add():
+    global todo_list_global
+    global count
+    count= count+1
+
+    title= request.form.get("title")
+
+    new_todo= Todo(num=count, title=title)
+    todo_list_global.append(new_todo)
+
+    return redirect("/")
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    global todo_list_global
+
+    for todo in todo_list_global:
+        if(todo.id == todo_id):
+            if (todo.complete):
+                todo.updateNotComplete()
+
+            else:
+                todo.updateComplete()
+    return redirect("/")
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    global todo_list_global
+
+    for todo in todo_list_global:
+        if(todo.id == todo_id):
+            todo.delete()
+            break
+    return redirect("/")
+
+
+
+        
